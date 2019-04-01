4e1b10cd6587198881a9d256ceaabe79b346d66d
JustWinDRunneR <evgeniytikhonov@ukr.net>
2019-04-01 23:21:47 +0300

    added jndi dependency

---
+++ src/main/java/justwindrunner/cinemaproject/com/github/Main.java
@@ -0,0 +1,38 @@
+package justwindrunner.cinemaproject.com.github;
+
+import org.apache.log4j.Logger;
+import org.osjava.sj.loader.SJDataSource;
+
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class Main {
+
+    public static final Logger LOGGER = Logger.getLogger(Main.class);
+
+    public static void main(String[] args) {
+        try {
+            DataSource dataSource = (SJDataSource) new InitialContext().lookup("db");
+            Connection connection = dataSource.getConnection();
+
+            String query = "SELECT * FROM cinema.movie;";
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            resultSet.next();
+
+            System.out.println(resultSet.getString("title"));
+
+        } catch (NamingException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
